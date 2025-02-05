b1a05a5220c4fc7fde50d06740d37abfefdc8ccf
Julio-orellana <julioes134@outlook.es>
2025-02-05 12:38:40 -0600

    Unit Tests

---
+++ src/test/java/org/sorting/ProfilerTest.java
@@ -0,0 +1,9 @@
+package org.sorting;
+
+import junit.framework.TestCase;
+
+public class ProfilerTest extends TestCase {
+
+    public void testName() {
+    }
+}
---
+++ src/test/java/org/sorting/SortingAlgorithmsTest.java
@@ -0,0 +1,31 @@
+package org.sorting;
+
+import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
+
+public class SortingAlgorithmsTest extends TestCase {
+
+    @Test
+    public void testInsertionSort() {
+    }
+
+    @Test
+    public void testMergeSort() {
+    }
+
+    @Test
+    public void testQuickSort() {
+    }
+
+    @Test
+    public void testRadixSort() {
+    }
+
+    @Test
+    public void testBucketSort() {
+    }
+
+    @Test
+    public void testHeapSort() {
+    }
+}
