29cd02dba909e40122791cfd4527f0885db58b97
Julio-orellana <julioes134@outlook.es>
2025-02-07 10:26:31 -0600

    Using matplot to show the results in a graph dinamicly

--- graph.py
+++ graph.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 import numpy as np
 
 times = pd.read_csv('results.csv')
@@ -12,3 +13,15 @@ radixSortTimes = np.array(times['RadixSort'])
 bucketSortTimes = np.array(times['BucketSort']) 
 heapSortTimes = np.array(times['HeapSort']) 
 
+plt.plot(arraySizes, insertionSortTimes, label='Insertion Sort')
+plt.plot(arraySizes, mergeSortTimes, label='Merge Sort')
+plt.plot(arraySizes, quickSortTimes, label='Quick Sort')
+plt.plot(arraySizes, radixSortTimes, label='Radix Sort')
+plt.plot(arraySizes, bucketSortTimes, label='Bucket Sort')
+plt.plot(arraySizes, heapSortTimes, label='Heap Sort')
+
+plt.xlabel('Array Size')
+plt.ylabel('Time (s)')
+plt.title('Sorting Algorithms Performance')
+plt.legend()
+plt.show()
