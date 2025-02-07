96e9e5995235897b53c6c582046957b5f6650b44
Julio-orellana <julioes134@outlook.es>
2025-02-07 10:46:54 -0600

    Adding info to the code

--- graph.py
+++ graph.py
@@ -6,6 +6,7 @@ times = pd.read_csv('results.csv')
 
 arraySizes = np.array(times['Size']) # Datos para el eje x
 
+# Tiempos de ejecucion de cada uno de los algoritmos.
 insertionSortTimes = np.array(times['InsertionSort'])
 mergeSortTimes = np.array(times['MergeSort'])
 quickSortTimes = np.array(times['QuickSort']) 
@@ -13,12 +14,13 @@ radixSortTimes = np.array(times['RadixSort'])
 bucketSortTimes = np.array(times['BucketSort']) 
 heapSortTimes = np.array(times['HeapSort']) 
 
-plt.plot(arraySizes, insertionSortTimes, label='Insertion Sort')
-plt.plot(arraySizes, mergeSortTimes, label='Merge Sort')
-plt.plot(arraySizes, quickSortTimes, label='Quick Sort')
-plt.plot(arraySizes, radixSortTimes, label='Radix Sort')
-plt.plot(arraySizes, bucketSortTimes, label='Bucket Sort')
-plt.plot(arraySizes, heapSortTimes, label='Heap Sort')
+# Graficar los tiempos de ejecucion de cada uno de los algoritmos.
+plt.plot(arraySizes, insertionSortTimes, label='Insertion Sort. O(n^2)')
+plt.plot(arraySizes, mergeSortTimes, label='Merge Sort. O(n log n)')
+plt.plot(arraySizes, quickSortTimes, label='Quick Sort. O(n log n)')
+plt.plot(arraySizes, radixSortTimes, label='Radix Sort. O(nk)')
+plt.plot(arraySizes, bucketSortTimes, label='Bucket Sort. O(n+k)')
+plt.plot(arraySizes, heapSortTimes, label='Heap Sort. O(n log n)')
 
 plt.xlabel('Array Size')
 plt.ylabel('Time (s)')
